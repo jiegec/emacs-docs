39683f2439fcd187e47caf96096356fafaa993bc
Jiege Chen <jiegec@qq.com>
2016-04-04 19:49:27 +0800

    Update update.rb

--- update.rb
+++ update.rb
@@ -1,12 +1,16 @@
 #!/usr/bin/env ruby
 require 'FileUtils'
 
-docs = ['lispintro/emacs-lisp-intro', 'lispref/elisp', 'emacs/emacs', 'misc/gnus', 'misc/tramp', 'misc/calc', 'misc/org']
 emacs = '/Volumes/Data/emacs'
+pwd = Dir.pwd
+Dir.chdir "#{emacs}/doc"
+docs = Dir.glob '**/*.html'
+Dir.chdir pwd
 
 docs.each do |doc|
-  unless (File.exist? "#{doc}.html") && (FileUtils.identical? "#{emacs}/doc/#{doc}.html", "#{doc}.html")
-    FileUtils.copy "#{emacs}/doc/#{doc}.html", "#{doc}.html"
-    puts "Updated #{doc}.html"
+  puts "Checking #{doc}"
+  unless (File.exist? doc) && (FileUtils.identical? "#{emacs}/doc/#{doc}", doc)
+    FileUtils.copy "#{emacs}/doc/#{doc}", doc
+    puts "Updated #{doc}"
   end
 end
