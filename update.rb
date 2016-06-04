5c2af1dc9efbae98675c995c7c502e840f999af2
Jiege Chen <jiegec@qq.com>
2016-06-04 20:36:37 +0800

    Remove pagebreak

--- update.rb
+++ update.rb
@@ -11,19 +11,18 @@ Dir.chdir pwd
 
 docs.each do |doc|
   # puts "Checking #{doc}"
-  unless (File.exist? "#{doc}.orig") && (FileUtils.identical? "#{emacs}/doc/#{doc}", "#{doc}.orig")
-    FileUtils.copy "#{emacs}/doc/#{doc}", "#{doc}.orig"
-    puts "Updated #{doc}"
-    file = Nokogiri::HTML(File.open("#{doc}.orig"))
-    nodes = file.search(".node")
-    nodes.each do |node|
-      node.before(file.create_element("mbp:pagebreak"))
-      link = node.css("a[name]")
-      node.children = link
-    end
-    File.open(doc, "w") { |f|
-      f.puts file
-    }
-    puts "Generating #{doc}"
+  # unless (File.exist? "#{doc}.orig") && (FileUtils.identical? "#{emacs}/doc/#{doc}", "#{doc}.orig")
+  FileUtils.copy "#{emacs}/doc/#{doc}", "#{doc}.orig"
+  puts "Updated #{doc}"
+  file = Nokogiri::HTML(File.open("#{doc}.orig"))
+  nodes = file.search(".node")
+  nodes.each do |node|
+    link = node.css("a[name]")
+    node.children = link
   end
+  File.open(doc, "w") { |f|
+    f.puts file
+  }
+  puts "Generating #{doc}"
+  # end
 end
